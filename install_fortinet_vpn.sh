7b6948097b09b0cd3b715374e4ea102d00dfc184
James Njovu <njovujames@gmail.com>
2024-03-07 09:02:44 +0200

    L

--- install_fortinet_vpn.sh
+++ install_fortinet_vpn.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 
-# wget -O - https://repo.fortinet.com/repo/7.0/ubuntu/DEB-GPG-KEY | sudo apt-key add -
+wget -O - https://repo.fortinet.com/repo/7.0/ubuntu/DEB-GPG-KEY | sudo apt-key add -
 
-# sudo sh -c 'echo "deb [arch=amd64] https://repo.fortinet.com/repo/7.0/ubuntu/ /bionic multiverse" >> /etc/apt/sources.list.d/forticlient.list'
+sudo sh -c 'echo "deb [arch=amd64] https://repo.fortinet.com/repo/7.0/ubuntu/ /bionic multiverse" >> /etc/apt/sources.list.d/forticlient.list'
 
-# sudo apt update
+sudo apt update
 
-# sudo apt install forticlient
+sudo apt install forticlient
 
 cd ~
-
-pwd
 
 wget http://cdn.software-mirrors.com/forticlientsslvpn_linux_4.4.2328.tar.gz
 
