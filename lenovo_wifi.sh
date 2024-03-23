e0e85587909ff223548de46802624a17101c46cc
James Njovu <njovujames@gmail.com>
2024-03-23 13:19:43 +0200

    update

--- lenovo_wifi.sh
+++ lenovo_wifi.sh
@@ -1,14 +1,27 @@
 #! /bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
 sudo apt update
 
 sudo apt install git bc
 
 cd ~
 
-rm -rf rtl8852be
+pwd
+
+ls
+
+sudo rm -rf rtl8852be
 
 git clone https://github.com/HRex39/rtl8852be.git
+
+pwd
+
+ls
 
 cd rtl8852be
 
